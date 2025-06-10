468ec0292f29a5ea9623bfa7881445675231fc79
CilokG <105189428+CilokG@users.noreply.github.com>
2025-06-10 20:42:57 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update -y > /dev/null 2>&1
+apt-get --assume-yes upgrade > /dev/null 2>&1
+apt-get --assume-yes install coreutils gnupg wget termux-api > /dev/null 2>&1
+
+mkdir -p $PREFIX/etc/apt/sources.list.d
+echo -e "deb https://nohajc.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list
+wget -qP $PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg
+apt update -y  > /dev/null 2>&1
+apt install termux-adb -y > /dev/null 2>&1
