14cb02ff626b88b877b551fef4f6927d4f831bf6
CilokG <105189428+CilokG@users.noreply.github.com>
2025-07-02 11:54:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pkg install termux-exec -y > /dev/null 2>&1
 
 apt-get update -y > /dev/null 2>&1
 apt-get --assume-yes upgrade > /dev/null 2>&1
-apt-get --assume-yes install coreutils gnupg wget termux-api > /dev/null 2>&1
+apt-get --assume-yes install coreutils gnupg wget clang termux-api > /dev/null 2>&1
 
 mkdir -p $PREFIX/etc/apt/sources.list.d
 echo -e "deb https://nohajc.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list
